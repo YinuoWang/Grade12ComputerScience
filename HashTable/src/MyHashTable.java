2d830ee4d0a467d586b9d254e1e195e2b3468d7a
YinuoWang <yinuo_wang@yahoo.ca>
2018-03-23 09:11:52 -0400

    Second HashTable Version
    Added search function, changed arraylist of arraylists to array of arraylists

--- HashTable/src/MyHashTable.java
+++ HashTable/src/MyHashTable.java
@@ -1,14 +1,14 @@
 import java.util.ArrayList;
 
 public class MyHashTable {
-    private ArrayList<ArrayList<StudentInfo>> buckets;
+    private ArrayList<StudentInfo>[] buckets;
     private int bucketCount;
 
     public MyHashTable(int k){
         bucketCount = k;
-        buckets = new ArrayList<ArrayList<StudentInfo>>(k);
+        buckets = new ArrayList[k];
         for (int i=0; i<k; ++i){
-            buckets.add(new ArrayList<StudentInfo>(k));
+            buckets[i] = new ArrayList<>();
         }
     }
 
@@ -17,32 +17,31 @@ public class MyHashTable {
     }
 
     public void addStudent(StudentInfo studentToBeAdded){
-        buckets.get(calcBucketNum(studentToBeAdded.getStudentNum())).add(studentToBeAdded);
+        buckets[calcBucketNum(studentToBeAdded.getStudentNum())].add(studentToBeAdded);
+    }
+
+    public int searchStudent(StudentInfo studentToBeAdded){
+        int bucketNum = calcBucketNum(studentToBeAdded.getStudentNum());
+        return buckets[bucketNum].indexOf(studentToBeAdded);
     }
 
     public void removeStudent(StudentInfo studentToBeAdded){
-        int bucketNum = calcBucketNum(studentToBeAdded.getStudentNum());
-        boolean flag = false;
-        for (int i=0; i<buckets.get(bucketNum).size(); ++i){
-            if (buckets.get(bucketNum).get(i).getStudentNum() == studentToBeAdded.getStudentNum()){
-                System.out.println("Student " + studentToBeAdded.getStudentNum() + " was removed.");
-                buckets.get(bucketNum).remove(i);
-                flag = true;
-                break;
-
-            }
-        }
-        if (!flag){
+        int position = searchStudent(studentToBeAdded);
+        if (position == -1){
             System.out.println("No student was removed.");
+        }
+        else{
+            buckets[calcBucketNum(studentToBeAdded.getStudentNum())].remove(position);
         }
     }
 
     public void printBuckets(){
         for (int i=0; i<bucketCount; ++i){
-            System.out.println("In bucket " + i + " there is:");
-            for (int k=0; k<buckets.get(i).size(); ++k){
-                System.out.println(buckets.get(i).get(k).getStudentNum());
+            System.out.print("In bucket " + i + " there is: ");
+            for (int k=0; k<buckets[i].size(); ++k){
+                System.out.print(buckets[i].get(k).getStudentNum() + " ");
             }
+            System.out.println();
         }
     }
 }
--- HashTable/src/StudentInfo.java
+++ HashTable/src/StudentInfo.java
@@ -37,5 +37,4 @@ public class StudentInfo {
             studentNum = sN;
         }
     }
-
 }
