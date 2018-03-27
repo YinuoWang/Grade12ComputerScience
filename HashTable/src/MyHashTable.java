0b0c52cae3e9df44e822609cf3f5417d89fca424
YinuoWang <yinuo_wang@yahoo.ca>
2018-03-27 10:06:16 -0400

    Changed function parameters and added comments

--- HashTable/src/MyHashTable.java
+++ HashTable/src/MyHashTable.java
@@ -1,9 +1,10 @@
 import java.util.ArrayList;
 
 public class MyHashTable {
-    private ArrayList<StudentInfo>[] buckets;
-    private int bucketCount;
+    private ArrayList<StudentInfo>[] buckets;       // Array of ArrayLists for to represent the HashTable
+    private int bucketCount;                        // The number of buckets in the HashTable
 
+    // Constructor for HashTable, parameter is the desired number of buckets
     public MyHashTable(int k){
         bucketCount = k;
         buckets = new ArrayList[k];
@@ -12,32 +13,47 @@ public class MyHashTable {
         }
     }
 
+    // Utility method for calculating which bucket in the HashTable the student should go into given the student number
     public int calcBucketNum(int sNum){
         return sNum % bucketCount;
     }
 
+    // Method for finding the StudentInfo object in the HashTable given their student number
+    public StudentInfo searchStudent(int studentNumberSearch){
+        int bucketNum = calcBucketNum(studentNumberSearch);                             // The bucket to search in
+        for (int i=0; i<buckets[bucketNum].size(); ++i){
+            if (buckets[bucketNum].get(i).getStudentNum() == studentNumberSearch){      // If a matching student # is found
+                return buckets[bucketNum].get(i);
+            }
+        }
+        return null;                                                                    // If no match is found
+    }
+
+    // Method for adding students to the HashTable
     public void addStudent(StudentInfo studentToBeAdded){
         buckets[calcBucketNum(studentToBeAdded.getStudentNum())].add(studentToBeAdded);
     }
 
-    public int searchStudent(StudentInfo studentToBeAdded){
-        int bucketNum = calcBucketNum(studentToBeAdded.getStudentNum());
-        return buckets[bucketNum].indexOf(studentToBeAdded);
+    // Method for removing students and returning the student removed
+    public StudentInfo removeStudent(int studentNumberSearch){
+        int bucketNum = calcBucketNum(studentNumberSearch);                                 // The bucket to search in
+        int position = buckets[bucketNum].indexOf(searchStudent(studentNumberSearch));      // Position of student in bucket
+        StudentInfo toBeReturned = null;                                                    // Variable to hold the return value
+        if (position != -1){                                                                // If the student is in the HashTable
+            toBeReturned = buckets[bucketNum].get(position);
+            buckets[bucketNum].remove(position);
+        }
+        return toBeReturned;
     }
 
-    public void removeStudent(StudentInfo studentToBeAdded){
-        int position = searchStudent(studentToBeAdded);
-        if (position == -1){
-            System.out.println("No student was removed.");
-        }
-        else{
-            buckets[calcBucketNum(studentToBeAdded.getStudentNum())].remove(position);
-        }
-    }
-
+    // Method for printing all the student numbers in the HashTable
     public void printBuckets(){
+
+        // Printing all student numbers in one bucket before moving onto the next bucket
         for (int i=0; i<bucketCount; ++i){
             System.out.print("In bucket " + i + " there is: ");
+
+            // Printing student numbers in a bucket in the order that they were added
             for (int k=0; k<buckets[i].size(); ++k){
                 System.out.print(buckets[i].get(k).getStudentNum() + " ");
             }
--- HashTable/src/TestHashTable.java
+++ HashTable/src/TestHashTable.java
@@ -1,17 +1,21 @@
 public class TestHashTable {
     public static void main(String[] args){
-        MyHashTable myHT = new MyHashTable(2);
+        MyHashTable myHT = new MyHashTable(2); // Defining and Instantiating a new HashTable
+
+        // Adding 5 students to the HashTable
         StudentInfo test1 = new StudentInfo(1, "bob", "smith");
-        StudentInfo test2 = new StudentInfo(2, "bob", "smith");
-        StudentInfo test3 = new StudentInfo(3, "bob", "smith");
-        StudentInfo test4 = new StudentInfo(4, "bob", "smith");
-        StudentInfo test5 = new StudentInfo(5, "bob", "smith");
+        StudentInfo test2 = new StudentInfo(2, "mike", "smith");
+        StudentInfo test3 = new StudentInfo(3, "dan", "smith");
+        StudentInfo test4 = new StudentInfo(4, "jim", "smith");
+        StudentInfo test5 = new StudentInfo(5, "sam", "smith");
         myHT.addStudent(test1);
         myHT.addStudent(test2);
         myHT.addStudent(test3);
         myHT.addStudent(test4);
+        myHT.addStudent(test5);
         myHT.printBuckets();
-        myHT.removeStudent(test1);
-        myHT.removeStudent(test5);
+        System.out.println(myHT.searchStudent(4).getFirstName());   // Testing search student method
+        System.out.println(myHT.removeStudent(1).getFirstName());   // Testing the removeStudent method
+        System.out.println(myHT.removeStudent(6));                  // Testing case if student is not in the HashTable
     }
 }
